07f2d9b90d998d99c989748ed9d20511720ff688
JoshRong <525732224@qq.com>
2021-08-24 21:33:34 +0800

    first Commit

---
+++ src/main/java/com/ronghao/gittest/GitTestApplication.java
@@ -0,0 +1,15 @@
+package com.ronghao.gittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitTestApplication.class, args);
+        System.out.println("==========================");
+        System.out.println("Hello Git");
+    }
+
+}
