57c7891861a6375e47e29440e92da816ff0a711c
JoshRong <525732224@qq.com>
2021-08-24 21:52:12 +0800

    hot-fix commit2

--- src/main/java/com/ronghao/gittest/GitTestApplication.java
+++ src/main/java/com/ronghao/gittest/GitTestApplication.java
@@ -14,6 +14,8 @@ public class GitTestApplication {
         System.out.println("Hello Git");
         System.out.println("Hello Git");
         System.out.println("Hello 哈哈哈哈");
+        System.out.println("Hello 哈哈哈哈2222");
     }
 
 }
+ 
